ba8857ae4050f6bb3d17b06fc674e891a7a5032d
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2025-03-02 13:03:47 +0530

    first commit

---
+++ prisma/migrations/20250302072357_user_and_todo_added/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "usrId" INTEGER NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_usrId_fkey" FOREIGN KEY ("usrId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
