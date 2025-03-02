e81c2f009b9e12b358cc8446b8eff96e26251770
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2025-03-02 14:21:37 +0530

    insert user I learned

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+async function insertUser(username : string , email : string , password : string , firstname : string  , lastname : string){
+  const res = await  prisma.user.create({
+        data : {
+            username : username,
+            email : email,
+            password : password,
+            firstname : firstname,
+            lastname : lastname,
+        } , 
+        select:{
+            id:true
+        }
+    })
+    console.log(res)
+}
+insertUser("anu Ruidas" , "anu@gmail.com" , "123456" , "anu" , "ruidas") 
